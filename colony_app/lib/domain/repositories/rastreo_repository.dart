07dc23e5739618b19a351055408a5a5493b1572f
Javier Cruz <321javiercruz@gmail.com>
2026-04-27 18:07:09 -0400

    limpieza profunda y creacion de rama deploy-prod

--- colony_app/lib/domain/repositories/rastreo_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../models/instancia_tracking.dart';
-
-abstract class RastreoRepository {
-  Future<InstanciaTracking> rastrear(String codigo);
-  Future<void> suscribirDispositivo(String codigo, String token);
-}
