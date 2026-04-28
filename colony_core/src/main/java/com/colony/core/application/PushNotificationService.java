cdbf42d5ffc609856a307676248ba1a6d618adf9
Javier Cruz <321javiercruz@gmail.com>
2026-04-28 16:57:37 -0400

    firebase para notificaciones para verificacion

--- colony_core/src/main/java/com/colony/core/application/PushNotificationService.java
+++ colony_core/src/main/java/com/colony/core/application/PushNotificationService.java
@@ -8,6 +8,9 @@ import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import com.google.firebase.messaging.BatchResponse;
+import com.google.firebase.messaging.SendResponse;
+
 @Slf4j
 @Service
 public class PushNotificationService {
@@ -28,9 +31,20 @@ public class PushNotificationService {
                     .addAllTokens(tokens)
                     .build();
 
-            FirebaseMessaging.getInstance().sendEachForMulticast(message);
+            BatchResponse response = FirebaseMessaging.getInstance().sendEachForMulticast(message);
+            
+            log.info("Notificaciones enviadas. Éxitos: {}, Fallos: {}", response.getSuccessCount(), response.getFailureCount());
+            
+            if (response.getFailureCount() > 0) {
+                List<SendResponse> responses = response.getResponses();
+                for (int i = 0; i < responses.size(); i++) {
+                    if (!responses.get(i).isSuccessful()) {
+                        log.error("Fallo al enviar a token [{}]: {}", tokens.get(i), responses.get(i).getException().getMessage());
+                    }
+                }
+            }
         } catch (Exception e) {
-            log.error("Error al enviar notificación Push: " + e.getMessage());
+            log.error("Error al intentar comunicarse con Firebase FCM: ", e);
         }
     }
 }
